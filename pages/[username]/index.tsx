e3c6881788109def8e56e24b6493b5e71212d107
Amir <iskandar.amir@gmail.com>
2023-02-22 19:42:56 +0800

    Use slider

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,16 +1,33 @@
+import Slider from "@/components/slider";
 import Head from "next/head";
 
 export default function UserRating() {
   const user = "Example User";
   const title = `Rate ${user}`;
+  const stats = [
+    { statName: "Looks" },
+    { statName: "Creativity" },
+    { statName: "Humor" },
+    { statName: "Mental Health" },
+    { statName: "Empathy" },
+    { statName: "Patience" },
+    { statName: "Social Skills" },
+    { statName: "Memory" },
+  ];
   return (
-    <>
+    <div className="grid gap-y-2">
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <h1 className="text-3xl text-center">Rate {user}</h1>
-    </>
+      <div className="grid gap-y-3">
+        {stats.map(({ statName }) => {
+          return <Slider statName={statName} />;
+        })}
+      </div>
+      <button className="border border-neutral-100">RATE!</button>
+    </div>
   );
 }
