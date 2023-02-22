9c0163be8e4e409db5c6e744b84b8f9cbf5e0b83
Amir <iskandar.amir@gmail.com>
2023-02-22 20:58:23 +0800

    Add supertokens wrapper to app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { frontendConfig } from "@/config/frontendConfig";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import SuperTokensReact, { SuperTokensWrapper } from "supertokens-auth-react";
+
+if (typeof window !== "undefined") {
+  // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
+  SuperTokensReact.init(frontendConfig());
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SuperTokensWrapper>
+      <Component {...pageProps} />
+    </SuperTokensWrapper>
+  );
 }
