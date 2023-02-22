04298fd8533ca65715429a7c4b16c4a12308f42a
Amir <iskandar.amir@gmail.com>
2023-02-23 01:21:10 +0800

    Update user info type to include ratings

--- types/userInfo.ts
+++ types/userInfo.ts
@@ -1,8 +1,14 @@
+export interface Rating {
+    statName: string,
+    self: number,
+    average: number
+}
 
 export interface UserInfo {
     username: string;
+    ratings: Array<Rating>
 }
-export const userInfoDefault = { username: "" }
+export const userInfoDefault = { username: "", ratings: [] }
 
 export interface AppContextInterface {
     userInfo: UserInfo;
