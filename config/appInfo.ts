25c0b6bd1f488be841e73689bddc1eb5554c3466
Amir <iskandar.amir@gmail.com>
2023-02-22 21:05:17 +0800

    Show login UI

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,8 +1,15 @@
+const port = process.env.APP_PORT || 3000
+
+const apiBasePath = '/api/auth/'
+
+export const websiteDomain =
+    process.env.APP_URL ||
+    process.env.NEXT_PUBLIC_APP_URL ||
+    `http://localhost:${port}`
+
 export const appInfo = {
-    // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
-    appName: "rate-me",
-    apiDomain: "http://localhost:3000",
-    websiteDomain: "http://localhost:3000",
-    apiBasePath: "/api/auth",
-    websiteBasePath: "/login"
+    appName: 'rate-me',
+    websiteDomain,
+    apiDomain: websiteDomain,
+    apiBasePath,
 }
