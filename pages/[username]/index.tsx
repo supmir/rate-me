335d7e780b23eb667dd1de1979515c9db58888d2
Amir <iskandar.amir@gmail.com>
2023-02-22 22:15:21 +0800

    Force sign in to rate user

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -2,6 +2,7 @@ import Slider from "@/components/slider";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { SessionAuth } from "supertokens-auth-react/recipe/session";
 
 export default function UserRating() {
   const router = useRouter();
@@ -25,26 +26,28 @@ export default function UserRating() {
   });
 
   return (
-    <div className="grid gap-y-4">
-      <Head>
-        <title>{title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+    <SessionAuth>
+      <div className="grid gap-y-4">
+        <Head>
+          <title>{title}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
 
-      <h1 className="text-3xl text-center">Rate {username}</h1>
-      <div className="grid gap-y-3">
-        {stats.map(({ statName, value, setValue }, i) => {
-          return (
-            <Slider
-              statName={statName}
-              value={value}
-              setValue={setValue}
-              key={i}
-            />
-          );
-        })}
+        <h1 className="text-3xl text-center">Rate {username}</h1>
+        <div className="grid gap-y-3">
+          {stats.map(({ statName, value, setValue }, i) => {
+            return (
+              <Slider
+                statName={statName}
+                value={value}
+                setValue={setValue}
+                key={i}
+              />
+            );
+          })}
+        </div>
+        <button className="border border-neutral-100">RATE!</button>
       </div>
-      <button className="border border-neutral-100">RATE!</button>
-    </div>
+    </SessionAuth>
   );
 }
