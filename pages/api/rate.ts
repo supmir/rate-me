d23f7e8b527e9891e9243349b4f6722d270ea3eb
Amir <iskandar.amir@gmail.com>
2023-02-23 00:42:28 +0800

    Mock rate API

---
+++ pages/api/rate.ts
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { firestore as db } from '@/lib/firebase-util'
+import { superTokensNextWrapper } from 'supertokens-node/nextjs'
+import { verifySession } from "supertokens-node/recipe/session/framework/express";
+import { SessionRequest } from "supertokens-node/framework/express";
+
+export default async function handler(
+    req: SessionRequest,
+    res: any
+) {
+
+
+    await superTokensNextWrapper(
+        async (next) => {
+            await verifySession()(req, res, next);
+        },
+        req,
+        res
+    )
+
+    let userId = req.session!.getUserId();
+    console.log(req.body)
+
+    // const { username } = req.query;
+    // const user = await db.collection("users").doc(userId).get()
+    // if (user.exists) {
+    //     res.status(200).json(user.data())
+    // } else {
+    //     await db.collection("users").doc(userId).set({})
+    //     res.status(200).json({})
+    // }
+    res.status(200).json({ "message": "bruh" })
+
+
+}
