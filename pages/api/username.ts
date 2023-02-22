066de761affcd536a99fb0aff53f4388f885700e
Amir <iskandar.amir@gmail.com>
2023-02-22 23:33:46 +0800

    Username set done

--- pages/api/username.ts
+++ pages/api/username.ts
@@ -1,21 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { firestore as db } from '@/lib/firebase-util'
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  message: string
-}
+import { superTokensNextWrapper } from 'supertokens-node/nextjs'
+import { verifySession } from "supertokens-node/recipe/session/framework/express";
+import { SessionRequest } from "supertokens-node/framework/express";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+  req: SessionRequest,
+  res: any
 ) {
+
+
+  await superTokensNextWrapper(
+    async (next) => {
+      await verifySession()(req, res, next);
+    },
+    req,
+    res
+  )
+
+  let userId = req.session!.getUserId();
+
+  console.log(userId)
   const { username } = req.query;
+  const user = await db.collection("users").doc(userId).get()
+  if (user.exists && (user.get("username") !== undefined)) {
+    res.status(422).json({ message: "Username has been set before" })
+    return
+  }
+
   const snapshot = await db.collection("users").where("username", "==", username).get()
 
   if (snapshot.empty) {
-
-    db.collection("users").doc("123").set({ username: username })
+    if (user.exists) {
+      db.collection("users").doc(userId).update({ username: username })
+    } else if (!user.exists) {
+      db.collection("users").doc(userId).set({ username: username })
+    }
     res.status(200).json({ message: "Username set" })
   } else {
     res.status(422).json({ message: "Username taken" })
