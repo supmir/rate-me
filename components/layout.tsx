6c0b2be7a83115eb9e144a044657be0cdf626042
Amir <iskandar.amir@gmail.com>
2023-02-23 13:11:52 +0800

    Add head into layout, also, adsense

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,25 @@
+import Head from "next/head";
 import Navbar from "./navbar";
 
 export default function Layout(props: any) {
   return (
     <div className="grid gap-y-2">
       <Navbar />
+
+      <Head>
+        <title>Mirror Rate</title>
+        <meta
+          name="description"
+          content="Rate yourself, then friends rate you!"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6871496328627568"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <div className="w-full max-w-2xl mx-auto p-2">{props.children}</div>
     </div>
   );
