dac9c7cc84c584b16ad1a85befeba9db9368c4fe
Amir <iskandar.amir@gmail.com>
2023-02-23 13:51:48 +0800

    Create share button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import { useAppContext } from "@/components/appWrapper";
 import Layout from "@/components/layout";
-import { CheckIcon } from "@heroicons/react/24/solid";
+import { sleep } from "@/lib/site";
+import {
+  CheckIcon,
+  ClipboardDocumentCheckIcon,
+  ClipboardDocumentIcon,
+} from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { MutableRefObject, useRef, useState } from "react";
 export default function Home() {
   const ref = useRef() as MutableRefObject<HTMLInputElement>;
   const { userInfo, session } = useAppContext();
   const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
   return (
     <Layout>
-      {/* TODO: Username selection */}
       <div className="flex h-full">
         {!session ? (
           <Link
@@ -30,6 +35,10 @@ export default function Home() {
               <button
                 className="border grid w-12 bg-neutral-100 text-neutral-900"
                 onClick={async () => {
+                  if (ref.current.value === "") {
+                    setUsernameMessage("This can't be empty");
+                    return;
+                  }
                   const resp = await fetch(
                     `/api/username?username=${ref.current.value}`
                   );
@@ -38,7 +47,6 @@ export default function Home() {
                   } else {
                     setUsernameMessage("Username taken");
                   }
-                  // TODO: refresh page error handling etc
                 }}
               >
                 <CheckIcon className="w-8 h-8 m-auto" />
@@ -46,12 +54,35 @@ export default function Home() {
             </div>
           </div>
         ) : (
-          <Link
-            href={`/@${userInfo.username}`}
-            className="text-3xl font-bold text-center border m-auto px-2 py-1"
-          >
-            <button>Rate yourself!</button>
-          </Link>
+          <div className="flex flex-col w-full gap-y-5 m-auto">
+            <Link
+              href={`/@${userInfo.username}`}
+              className="text-3xl font-bold text-center border px-2 py-1 mx-auto"
+            >
+              <button>Rate yourself!</button>
+            </Link>
+            <div className="flex flex-col">
+              <div className="mx-auto">Share your profile to your friends!</div>
+              <button
+                className="border p-2 rounded-xl mx-auto flex gap-x-2 select-none"
+                onClick={async () => {
+                  navigator.clipboard.writeText(
+                    `https://mirrorrate.vercel.app/@${userInfo.username}`
+                  );
+                  setIsCopied(true);
+                  await sleep(3000);
+                  setIsCopied(false);
+                }}
+              >
+                {`https://mirrorrate.vercel.app/@${userInfo.username}`}
+                {isCopied ? (
+                  <ClipboardDocumentCheckIcon className="h-6 w-6" />
+                ) : (
+                  <ClipboardDocumentIcon className="h-6 w-6" />
+                )}
+              </button>
+            </div>
+          </div>
         )}
         {/* TODO: link share */}
         {/* <div>
