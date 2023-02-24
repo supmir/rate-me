daf31bfce0bd783a07e7582cd026b30f50962126
Amir <amir.iskandar@mnrb.com.my>
2023-02-24 13:02:33 +0800

    Better nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 import { doesSessionExist } from "supertokens-website";
 import { useAppContext } from "@/components/appWrapper";
@@ -28,22 +28,29 @@ export default function Navbar() {
   return (
     <div className="flex justify-between bg-black py-2 px-4 h-16">
       <Link href="/" className="my-auto">
-        <div>@{userInfo.username}</div>
+        <div>Mirror Rate</div>
       </Link>
-      {session ? (
-        <button
-          className="border border-neutral-100 px-2 py-1"
-          onClick={clickSignOut}
-        >
-          Sign Out
-        </button>
-      ) : (
-        <Link href="/auth">
-          <button className="border border-neutral-100 px-2 py-1">
-            Sign In
-          </button>
-        </Link>
-      )}
+      <div className="flex my-auto gap-x-2">
+        {session ? (
+          <Fragment>
+            <Link href="/" className="border border-neutral-100 px-2 py-1">
+              <div>@{userInfo.username}</div>
+            </Link>
+            <button
+              className="border border-neutral-100 px-2 py-1"
+              onClick={clickSignOut}
+            >
+              Sign Out
+            </button>
+          </Fragment>
+        ) : (
+          <Link href="/auth">
+            <button className="border border-neutral-100 px-2 py-1">
+              Sign In
+            </button>
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
