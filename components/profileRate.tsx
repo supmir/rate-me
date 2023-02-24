aebeeb978e4bf53a4b5039f0263740b17e60ac89
Amir <iskandar.amir@gmail.com>
2023-02-24 14:04:03 +0800

    Put a container around profile rate component

--- components/profileRate.tsx
+++ components/profileRate.tsx
@@ -5,12 +5,12 @@ import Bar from "./bar";
 export default function ProfileRate(props: { userInfo: UserInfo }) {
   const { userInfo } = props;
   return (
-    <Fragment>
+    <div className="flex flex-col gap-x-2">
       {userInfo.ratings.map(({ statName, self, average }, i) => {
         return (
           <Bar statName={statName} self={self} average={average} key={i} />
         );
       })}
-    </Fragment>
+    </div>
   );
 }
