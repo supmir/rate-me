c44c9a69ac99383138be6a94df8b48e3678e2390
Amir <iskandar.amir@gmail.com>
2023-03-02 09:26:49 +0800

    Put try catch

--- pages/api/rate.ts
+++ pages/api/rate.ts
@@ -10,59 +10,61 @@ export default async function handler(
     req: SessionRequest,
     res: any
 ) {
+    try {
 
+        await superTokensNextWrapper(
+            async (next) => {
+                await verifySession()(req, res, next);
+            },
+            req,
+            res
+        )
 
-    await superTokensNextWrapper(
-        async (next) => {
-            await verifySession()(req, res, next);
-        },
-        req,
-        res
-    )
+        let userId = req.session!.getUserId();
+        const payload = JSON.parse(req.body)
+        const snapshot = await db.collection("users").where("username_lowercase", "==", payload.targetUser.toLowerCase()).get()
+        const targetUser = snapshot.docs[0]
+        const targetUserId = targetUser.id
 
-    let userId = req.session!.getUserId();
-    const payload = JSON.parse(req.body)
-    const snapshot = await db.collection("users").where("username_lowercase", "==", payload.targetUser.toLowerCase()).get()
-    const targetUser = snapshot.docs[0]
-    const targetUserId = targetUser.id
-
-    let ratings: { [key: string]: number } = {}
-    for (const statName of statsList) {
-        ratings[statName] = payload.rating.find((rate: Rating) => {
-            return rate.statName === statName
-        }).value
-    }
-    const data = {
-        byUserId: userId,
-        targetUserId: targetUserId,
-        ratings: ratings,
-        createdAt: Timestamp.now()
-    }
-
-    await db.collection("ratings").add(data)
-
-    if (userId === targetUserId) {
-        db.collection("users").doc(userId).update({
-            self: ratings,
-            modifiedAt: Timestamp.now(),
-        })
-    } else {
-        const count = targetUser.data().count || 0
-        let new_average: { [key: string]: number } = {}
+        let ratings: { [key: string]: number } = {}
         for (const statName of statsList) {
-            const cur_av = targetUser.data().average[statName] ? targetUser.data().average[statName] : 0
-
-            new_average[statName] = (ratings[statName] + (cur_av * count)) / (count + 1)
+            ratings[statName] = payload.rating.find((rate: Rating) => {
+                return rate.statName === statName
+            }).value
         }
-        db.collection("users").doc(targetUserId).update({
-            average: new_average,
-            count: count + 1,
-            modifiedAt: Timestamp.now(),
-        })
+        const data = {
+            byUserId: userId,
+            targetUserId: targetUserId,
+            ratings: ratings,
+            createdAt: Timestamp.now()
+        }
+
+        await db.collection("ratings").add(data)
+
+        if (userId === targetUserId) {
+            db.collection("users").doc(userId).update({
+                self: ratings,
+                modifiedAt: Timestamp.now(),
+            })
+        } else {
+            const count = targetUser.data().count || 0
+            let new_average: { [key: string]: number } = {}
+            for (const statName of statsList) {
+                const cur_av = targetUser.data().average[statName] ? targetUser.data().average[statName] : 0
+
+                new_average[statName] = (ratings[statName] + (cur_av * count)) / (count + 1)
+            }
+            db.collection("users").doc(targetUserId).update({
+                average: new_average,
+                count: count + 1,
+                modifiedAt: Timestamp.now(),
+            })
+
+        }
+
+        res.status(200).json({ "message": "Rating received" })
+    } catch {
+        res.status(500).json({ "message": "An error has occured" })
 
     }
-
-    res.status(200).json({ "message": "bruh" })
-
-
 }
